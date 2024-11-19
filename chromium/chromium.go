a26ec3f91b0f0d8c037839c87da3ea397e67b600
Xotam <fobus1289@gmail.com>
2024-11-20 01:09:57 +0500

    fix

---
+++ chromium/chromium.go
@@ -0,0 +1,163 @@
+package chromium
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/chromedp/cdproto/page"
+	"github.com/chromedp/chromedp"
+)
+
+type ChromiumService interface {
+	GenSvg(ctx context.Context, url string) (string, error)
+	Run(writer io.Writer, url string) (string, error)
+	Close()
+}
+
+type chromiumService struct {
+	allocCtx    context.Context
+	allocCancel context.CancelFunc
+
+	chromedpCtx    context.Context
+	chromedpCancel context.CancelFunc
+}
+
+func New() (ChromiumService, error) {
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.NoSandbox,
+		chromedp.DisableGPU,
+		chromedp.Headless,
+	)
+
+	allocCtx, allocCancel := chromedp.NewExecAllocator(context.Background(), opts...)
+
+	chromedpCtx, chromedpCancel := chromedp.NewContext(allocCtx) // chromedp.WithDebugf(log.Printf),
+
+	if err := chromedp.Run(chromedpCtx); err != nil {
+		return nil, err
+	}
+
+	return &chromiumService{
+		allocCtx:       allocCtx,
+		allocCancel:    allocCancel,
+		chromedpCtx:    chromedpCtx,
+		chromedpCancel: chromedpCancel,
+	}, nil
+}
+
+func (c *chromiumService) Close() {
+	c.allocCancel()
+	c.chromedpCancel()
+}
+
+func (c *chromiumService) GenSvg(_ context.Context, url string) (string, error) {
+	var outer string
+	{
+		now := time.Now()
+		ctx, cancel := chromedp.NewContext(c.chromedpCtx)
+		defer cancel()
+
+		err := chromedp.Run(ctx,
+			chromedp.Navigate("about:blank"),
+
+			chromedp.ActionFunc(func(ctx context.Context) error {
+				lctx, cancel := context.WithCancel(ctx)
+				defer cancel()
+				var wg sync.WaitGroup
+				wg.Add(1)
+				chromedp.ListenTarget(lctx, func(ev interface{}) {
+					if _, ok := ev.(*page.EventLoadEventFired); ok {
+						cancel()
+						wg.Done()
+					}
+				})
+
+				frameTree, err := page.GetFrameTree().Do(ctx)
+				if err != nil {
+					return err
+				}
+
+				if err := page.SetDocumentContent(frameTree.Frame.ID, url).Do(ctx); err != nil {
+					return err
+				}
+				wg.Wait()
+				return nil
+			}),
+
+			chromedp.Click(`#canvas`, chromedp.ByQuery),
+			chromedp.WaitVisible(`#svg`),
+			chromedp.OuterHTML("#svg", &outer, chromedp.ByQuery),
+		)
+
+		if err != nil {
+			return "", err
+		}
+
+		log.Println(time.Since(now))
+	}
+
+	return outer, nil
+}
+
+func (c *chromiumService) Run(writer io.Writer, url string) (string, error) {
+	now := time.Now()
+
+	// Создаем контекст для chromedp
+	ctx, cancel := chromedp.NewContext(c.chromedpCtx)
+	defer cancel()
+
+	// Основная задача
+	if err := chromedp.Run(ctx,
+		chromedp.Navigate("about:blank"),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			lctx, cancel := context.WithCancel(ctx)
+			defer cancel()
+			var once sync.Once
+
+			chromedp.ListenTarget(lctx, func(ev any) {
+				if _, ok := ev.(*page.EventLoadEventFired); ok {
+					once.Do(func() {
+						cancel()
+					})
+				}
+			})
+
+			frameTree, err := page.GetFrameTree().Do(ctx)
+			{
+				if err != nil {
+					return err
+				}
+
+				if err := page.SetDocumentContent(frameTree.Frame.ID, url).Do(ctx); err != nil {
+					return err
+				}
+			}
+
+			return nil
+		}),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			buf, _, err := page.PrintToPDF().
+				WithPrintBackground(false).
+				WithLandscape(true).
+				Do(ctx)
+			if err != nil {
+				return err
+			}
+
+			if _, err := io.Copy(writer, bytes.NewReader(buf)); err != nil {
+				return err
+			}
+			return nil
+		}),
+	); err != nil {
+		return "", err
+	}
+
+	// Логируем время выполнения
+	log.Printf("Time taken: %v\n", time.Since(now))
+	return "", nil
+}
--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.23.0
 
 require (
 	github.com/caarlos0/env/v11 v11.2.2
+	github.com/chromedp/cdproto v0.0.0-20241022234722-4d5d5faf59fb
+	github.com/chromedp/chromedp v0.11.2
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/redis/go-redis/v9 v9.7.0
+	github.com/xuri/excelize/v2 v2.9.0
 	golang.org/x/exp v0.0.0-20241004190924-225e2abe05e6
 	gorm.io/driver/postgres v1.5.9
 	gorm.io/gorm v1.25.12
@@ -15,18 +18,29 @@ require (
 
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/chromedp/sysutil v1.1.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.1 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/xuri/efp v0.0.0-20240408161823-9ad904a10d6d // indirect
+	github.com/xuri/nfp v0.0.0-20240318013403-ab9948c2c4a7 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
